eb6c271b3e67c6d2889d60074cdf4003a2ad643c
Pedro Paulo <pedro98seabra@gmail.com>
2023-10-03 10:17:29 -0300

    Adiciona a data de atualização 2023-10-03 10:17:29

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
+import datetime
+import os
+import subprocess
+import schedule
+import time
 
-Data de atualização: 2023-10-03 10:16:50
+print('App on...')
+
+def commit_and_push():
+    # Define o caminho para o arquivo main.txt
+    path = "/home/the-pedrosantana/pessoal/automated-commiter/main.txt"
+
+    print("Print 1")
+    # Pega a data e hora atuais
+    now = datetime.datetime.now()
+
+    print("Print 2")
+    # Cria a string com a data e hora no formato desejado
+    date_string = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    print("Print 3")
+    # Adiciona a data ao arquivo main.txt
+    with open(path, "w") as file:
+        print("Print 4")
+        file.write(f"\nData de atualização: {date_string}\n")
+
+    print("Arquivo atualizado com sucesso!")
+    # Adiciona as mudanças ao Git e faz commit
+    os.system("git add .")
+    os.system(f'git commit -m "Adiciona a data de atualização {date_string}"')
+
+    # Faz push das mudanças para o repositório remoto
+    subprocess.run("git push origin main")
+    print("Mudanças enviadas ao github com sucesso!")
+
+# Agenda a execução do código a cada 1 minutos
+schedule.every(10).seconds.do(commit_and_push)
+
+while True:
+    print('Loading...')
+    schedule.run_pending()
+    time.sleep(1)
